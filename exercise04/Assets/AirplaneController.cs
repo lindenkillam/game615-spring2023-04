b2f2a767a928e87f2c7d37365b3993b000950924
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2023-02-13 02:03:22 -0500

    Airplane now pitches and rolls

--- exercise04/Assets/AirplaneController.cs
+++ exercise04/Assets/AirplaneController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class AirplaneController : MonoBehaviour
 {
+    GameObject airplane;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        airplane = GameObject.Find("Airplane");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            airplane.transform.Rotate(0, 0, 0.01f);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            airplane.transform.Rotate(0, 0, -0.01f);
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            airplane.transform.Rotate(0.003f, 0, 0);
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            airplane.transform.Rotate(-0.003f, 0, 0);
+        }
     }
 }
