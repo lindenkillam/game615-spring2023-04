73b3622364d1685b31d47cce28745ce81000faea
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2023-02-13 20:46:33 -0500

    It flies!

--- exercise04/Assets/AirplaneController.cs
+++ exercise04/Assets/AirplaneController.cs
@@ -5,32 +5,47 @@ using UnityEngine;
 public class AirplaneController : MonoBehaviour
 {
     GameObject airplane;
+    Rigidbody rb;
+    float rotateSpeed, rotateRate, pitchRate, forwardSpeed;
+    //float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         airplane = GameObject.Find("Airplane");
+        rotateSpeed = 0f;
+        rotateRate = 0.01f;
+        pitchRate = 0.03f;
+        forwardSpeed = 1f;
+        //rb = airplane.GetComponent<Rigidbody>();
+        //speed = 100.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        airplane.transform.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.Self);
+        airplane.transform.Translate(transform.forward * forwardSpeed * Time.deltaTime, Space.World);
+        //rb.velocity = transform.forward * speed;
+
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            airplane.transform.Rotate(0, 0, 0.01f);
+            airplane.transform.Rotate(0, 0, rotateRate);
+            rotateSpeed -= rotateRate;
         }
         else if(Input.GetKey(KeyCode.RightArrow))
         {
-            airplane.transform.Rotate(0, 0, -0.01f);
+            airplane.transform.Rotate(0, 0, -rotateRate);
+            rotateSpeed += rotateRate;
         }
 
         if(Input.GetKey(KeyCode.UpArrow))
         {
-            airplane.transform.Rotate(0.003f, 0, 0);
+            airplane.transform.Rotate(pitchRate, 0, 0);
         }
         else if(Input.GetKey(KeyCode.DownArrow))
         {
-            airplane.transform.Rotate(-0.003f, 0, 0);
+            airplane.transform.Rotate(-pitchRate, 0, 0);
         }
     }
 }
